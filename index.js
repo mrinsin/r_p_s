334a6339265bcafba6487f44886e03d16c696f57
Mrinalini Sinha <ms6471@nyu.edu>
2017-03-01 17:00:56 -0800

    wrote jquery for the actual game

--- index.js
+++ index.js
@@ -7,127 +7,142 @@ var userPlay = "";
 //load the page when the document is ready
 $(document).ready(function(){
 
+  letsPLay();
 
-  //Let the console user know the game has started
-  console.log("The game starts");
-
-  //when the user clicks on the rock image,
-  $("#rock").on("click", function(){
-
-    console.log("rock is the user's pick.\n");
-    userPlay = "Rock";
-
-    //lets the machine pick a choice to play
-    machineChoice();
-
-    //let me know what choice the machine made
-    console.log(myChoice + " is my pick.\n");
-
-    //if both our choices math, it is a tie
-    if (userPlay === myChoice) {
-      $("#winner").text("Tie! No one wins.");
-      console.log("Tie");
-      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
-    }
-
-
-    //Winning connditions for the user
-    if (myChoice === "Scissors") {
-      $("#winner").text("Hooray! You win.");
-      console.log("User wins");
-      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
-    }
-
-
-    //winning conditions for the winner
-    if (myChoice === "Paper") {
-      $("#winner").text("Lol I win. Now I will take over your country.");
-      console.log("I win");
-      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
-    }
-
-    myChoice = " ";
-
-  });
-
-  //when the user clicks on paper,
-  $("#paper").on("click", function(){
-
-    console.log("Paper is the user's pick.\n");
-    userPlay = "Paper"
-
-    //lets the machine pick a choice to play
-    machineChoice();
-
-    //let me know what choice the machine made
-    console.log(myChoice + " is my pick.\n");
-
-    //if both our choices math, it is a tie
-    if (userPlay === myChoice) {
-      $("#winner").text("Tie! No one wins.");
-      console.log("Tie");
-      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
-    }
-
-
-    //Winning connditions for the user
-    if (myChoice === "Rock") {
-      $("#winner").text("Hooray! You win.");
-      console.log("User wins");
-      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
-    }
-
-
-    //winning conditions for the winner
-    if (myChoice === "Scissors") {
-      $("#winner").text("Lol I win. Now I will take over your country.");
-      console.log("I win");
-      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
-    }
-
-    myChoice = " ";
-
-  });
-
-  //when the user clicks on scissors,
-  $("#scissors").on("click", function(){
-
-    console.log("Scissors is the user's pick.\n");
-    userPlay = "Scissors"
-
-    //lets the machine pick a choice to play
-    machineChoice();
-
-    //let me know what choice the machine made
-    console.log(myChoice + " is my pick.\n");
-
-    //if both our choices math, it is a tie
-    if (userPlay === myChoice) {
-      $("#winner").text("Tie! No one wins.");
-      console.log("Tie");
-      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
-    }
-
-
-    //Winning connditions for the user
-    if (myChoice === "Paper") {
-      $("#winner").text("Hooray! You win.");
-      console.log("User wins");
-      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
-    }
-
-
-    //winning conditions for the winner
-    if (myChoice === "Rock") {
-      $("#winner").text("Lol I win. Now I will take over your country.");
-      console.log("I win");
-      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
-    }
-
-    myChoice = " ";
-
-  });
 });
+
+//The function that starts the games
+function letsPLay() {
+
+    //Let the console user know the game has started
+    console.log("The game starts");
+
+    //when the user clicks on the rock image,
+    $("#rock").on("click", function(){
+
+      console.log("rock is the user's pick.\n");
+      userPlay = "Rock";
+
+      //lets the machine pick a choice to play
+      machineChoice();
+
+      //let me know what choice the machine made
+      console.log(myChoice + " is my pick.\n");
+
+      //if both our choices math, it is a tie
+      if (userPlay === myChoice) {
+        $("#center").replaceWith('<h1 id="result">Tie! No one wins.</h1>');
+        console.log("Tie");
+        $("#left").replaceWith('<img src="rock.jpeg" />');
+        $("#right").replaceWith('<img src="rock.jpeg" />');
+      }
+
+
+      //Winning connditions for the user
+      if (myChoice === "Scissors") {
+        $("#center").replaceWith('<h1 id="result">Hooray! You win.</h1>');
+        console.log("User wins boooo");
+        $("#left").replaceWith('<img src="rock.jpeg" />');
+        $("#right").replaceWith('<img src="scissors.png" />');
+      }
+
+
+      //winning conditions for the winner
+      if (myChoice === "Paper") {
+        $("#center").replaceWith('<h1 id="result">HAHAHAHHA I win!</h1>');
+        console.log("Yesssss I win");
+        $("#left").replaceWith('<img src="rock.jpeg" />');
+        $("#right").replaceWith('<img src="paper.jpeg" />');
+      }
+
+      myChoice = " ";
+
+    });
+
+    //when the user clicks on paper,
+    $("#paper").on("click", function(){
+
+      console.log("Paper is the user's pick.\n");
+      userPlay = "Paper"
+
+      //lets the machine pick a choice to play
+      machineChoice();
+
+      //let me know what choice the machine made
+      console.log(myChoice + " is my pick.\n");
+
+      //if both our choices math, it is a tie
+      if (userPlay === myChoice) {
+        $("#center").replaceWith('<h1 id="result">Tie! No one wins.</h1>');
+        console.log("Tie");
+        $("#left").replaceWith('<img src="paper.jpeg" />');
+        $("#right").replaceWith('<img src="paper.jpeg" />');
+      }
+
+
+      //Winning connditions for the user
+      if (myChoice === "Rock") {
+        $("#center").replaceWith('<h1 id="result">Hooray! You win.</h1>');
+        console.log("User wins boooo");
+        $("#left").replaceWith('<img src="paper.jpeg" />');
+        $("#right").replaceWith('<img src="rock.jpeg" />');
+      }
+
+
+      //winning conditions for the winner
+      if (myChoice === "Scissors") {
+        $("#center").replaceWith('<h1 id="result">HAHAHAHHA I win!</h1>');
+        console.log("Yesssss I win");
+        $("#left").replaceWith('<img src="paper.jpeg" />');
+        $("#right").replaceWith('<img src="scissors.png" />');
+      }
+
+      myChoice = " ";
+
+    });
+
+    //when the user clicks on scissors,
+    $("#scissors").on("click", function(){
+
+      console.log("Scissors is the user's pick.\n");
+      userPlay = "Scissors"
+
+      //lets the machine pick a choice to play
+      machineChoice();
+
+      //let me know what choice the machine made
+      console.log(myChoice + " is my pick.\n");
+
+      //if both our choices math, it is a tie
+      if (userPlay === myChoice) {
+        $("#center").replaceWith('<h1 id="result">Tie! No one wins.</h1>');
+        console.log("Tie");
+        $("#left").replaceWith('<img src="scissors.png" />');
+        $("#right").replaceWith('<img src="scissors.png" />');
+      }
+
+
+      //Winning connditions for the user
+      if (myChoice === "Paper") {
+        $("#center").replaceWith('<h1 id="result">Hooray! You win.</h1>');
+        console.log("User wins boooo");
+        $("#left").replaceWith('<img src="scissors.png" />');
+        $("#right").replaceWith('<img src="paper.jpeg" />');
+      }
+
+
+      //winning conditions for the winner
+      if (myChoice === "Rock") {
+        $("#center").replaceWith('<h1 id="result">HAHAHAHHA I win!</h1>');
+        console.log("Yesssss I win");
+        $("#left").replaceWith('<img src="scissors.png" />');
+        $("#right").replaceWith('<img src="rock.jpeg" />');
+      }
+
+      myChoice = " ";
+
+    });
+}
 
 //generates a random choice for the machine, limited to rock/paper/scissors
 function machineChoice() {
