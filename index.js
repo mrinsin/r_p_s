4d63360af611aa15754484d66cbf743cb1c944a1
Mrinalini Sinha <ms6471@nyu.edu>
2017-03-01 16:35:28 -0800

    improved algorithm for computers choice

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ var userPlay = "";
 //load the page when the document is ready
 $(document).ready(function(){
 
+
   //Let the console user know the game has started
   console.log("The game starts");
 
-  //when the user clicks the play button, the following happens.
+  //when the user clicks on the rock image,
   $("#rock").on("click", function(){
 
-    console.log(" roock is the user's pick.\n");
-    userPlay = "Rock"
+    console.log("rock is the user's pick.\n");
+    userPlay = "Rock";
 
     //lets the machine pick a choice to play
     machineChoice();
@@ -33,6 +34,7 @@ $(document).ready(function(){
     //Winning connditions for the user
     if (myChoice === "Scissors") {
       $("#winner").text("Hooray! You win.");
+      console.log("User wins");
       $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
     }
 
@@ -40,26 +42,106 @@ $(document).ready(function(){
     //winning conditions for the winner
     if (myChoice === "Paper") {
       $("#winner").text("Lol I win. Now I will take over your country.");
+      console.log("I win");
       $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
     }
 
+    myChoice = " ";
+
   });
 
+  //when the user clicks on paper,
+  $("#paper").on("click", function(){
+
+    console.log("Paper is the user's pick.\n");
+    userPlay = "Paper"
+
+    //lets the machine pick a choice to play
+    machineChoice();
+
+    //let me know what choice the machine made
+    console.log(myChoice + " is my pick.\n");
+
+    //if both our choices math, it is a tie
+    if (userPlay === myChoice) {
+      $("#winner").text("Tie! No one wins.");
+      console.log("Tie");
+      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
+    }
+
+
+    //Winning connditions for the user
+    if (myChoice === "Rock") {
+      $("#winner").text("Hooray! You win.");
+      console.log("User wins");
+      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
+    }
+
+
+    //winning conditions for the winner
+    if (myChoice === "Scissors") {
+      $("#winner").text("Lol I win. Now I will take over your country.");
+      console.log("I win");
+      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
+    }
+
+    myChoice = " ";
+
+  });
+
+  //when the user clicks on scissors,
+  $("#scissors").on("click", function(){
+
+    console.log("Scissors is the user's pick.\n");
+    userPlay = "Scissors"
+
+    //lets the machine pick a choice to play
+    machineChoice();
+
+    //let me know what choice the machine made
+    console.log(myChoice + " is my pick.\n");
+
+    //if both our choices math, it is a tie
+    if (userPlay === myChoice) {
+      $("#winner").text("Tie! No one wins.");
+      console.log("Tie");
+      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
+    }
+
+
+    //Winning connditions for the user
+    if (myChoice === "Paper") {
+      $("#winner").text("Hooray! You win.");
+      console.log("User wins");
+      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
+    }
+
+
+    //winning conditions for the winner
+    if (myChoice === "Rock") {
+      $("#winner").text("Lol I win. Now I will take over your country.");
+      console.log("I win");
+      $("#playerResults").text("You played : " + userPlay + " || The machine played : " + myChoice + " .");
+    }
+
+    myChoice = " ";
+
+  });
 });
 
 //generates a random choice for the machine, limited to rock/paper/scissors
 function machineChoice() {
 
   //this variable can store 6 different numbers
-  var play = Math.floor(Math.random()*6);
+  var play = Math.floor(Math.random()*3);
 
 //for each par out of the 6 different numbers, I assign a play choice to the variable myChoice.
 
-  if (play === 0 || play === 4) {
+  if (play === 0) {
     myChoice = "Rock";
-  } else if (play === 1 || play === 3) {
+  } else if (play === 1) {
     myChoice = "Paper";
-  } else if (play === 2 || play === 5) {
+  } else if (play === 2) {
     myChoice = "Scissors";
   }
 }
